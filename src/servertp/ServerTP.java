724fcc7e306e6db47b6898cf3abdd0ca4713d415
gastonmira <gastonmira@gmail.com>
2016-04-21 17:17:30 -0300

    Initial commit

---
+++ src/servertp/ServerTP.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servertp;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gaston.mira
+ */
+public class ServerTP {
+
+    //private ServerSocket serverSocket;
+    private ArrayList<String> listaNoticias = new ArrayList<>();
+    
+    
+    public void showNoticias() {
+        System.out.println("Lista de Noticias del Servidor");
+        for(int i=0; i<listaNoticias.size();i++) {
+            System.out.println(listaNoticias.get(i));
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        ServerTP server = new ServerTP();
+        server.run();
+    }
+    
+    public void run() throws Exception {
+        int port = 6006;
+        System.out.println("Servidor Escuchando por puerto " + String.valueOf(port));
+        ServerSocket server = new ServerSocket(port);
+        server.setSoTimeout(10000000);
+        Socket clientSocket = server.accept();
+        InputStreamReader IR = new InputStreamReader(clientSocket.getInputStream());
+        BufferedReader BR = new BufferedReader(IR);
+        
+        String message = BR.readLine();
+        System.out.println(message);
+        
+        if(message != null) {
+            PrintStream PS = new PrintStream(clientSocket.getOutputStream());
+            PS.println("Mensaje Recibido!");
+        }
+    
+    
+    }
+    
+}
