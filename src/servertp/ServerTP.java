00a3985fac78d4734670a9039abcd51368981c9a
juan.dapice <juan.dapice@ar-it01779.globant.com>
2016-04-23 00:14:52 -0300

    Agregada Clase notica, Refactor clase server

--- src/servertp/ServerTP.java
+++ src/servertp/ServerTP.java
@@ -24,42 +24,31 @@ public class ServerTP {
 
     //private ServerSocket serverSocket;
     private ArrayList<String> listaNoticias = new ArrayList<>();
-    
-    
-    public void showNoticias() {
-        System.out.println("Lista de Noticias del Servidor");
-        for(int i=0; i<listaNoticias.size();i++) {
-            System.out.println(listaNoticias.get(i));
-        }
-    }
-    
+
+//    public void showNoticias() {
+//        System.out.println("Lista de Noticias del Servidor");
+//        for (int i = 0; i < listaNoticias.size(); i++) {
+//            System.out.println(listaNoticias.get(i));
+//        }
+//    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception{
-        // TODO code application logic here
-        ServerTP server = new ServerTP();
-        server.run();
-    }
-    
-    public void run() throws Exception {
+    public static void main(String[] args) throws Exception {
+
         int port = 6006;
-        System.out.println("Servidor Escuchando por puerto " + String.valueOf(port));
         ServerSocket server = new ServerSocket(port);
-        server.setSoTimeout(10000000);
-        Socket clientSocket = server.accept();
-        InputStreamReader IR = new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader BR = new BufferedReader(IR);
-        
-        String message = BR.readLine();
-        System.out.println(message);
-        
-        if(message != null) {
-            PrintStream PS = new PrintStream(clientSocket.getOutputStream());
-            PS.println("Mensaje Recibido!");
+        System.out.println("Servidor Escuchando por puerto " + String.valueOf(port));
+
+        while (true) {
+            Socket clientSocket = server.accept();
+            InputStreamReader IR = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader BR = new BufferedReader(IR);
+            String message = BR.readLine();
+            System.out.println(message);
+
+            //Chequeamos que nos pidio el cliente y contestamos acorde
         }
-    
-    
     }
-    
 }
